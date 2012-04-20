29350346b65efeba0360e6539e736d404bf854a7
Robert Calise <rcalise@mediajitsu.com>
2012-04-20 13:54:09 -0700

    Initial WP Plugin Comments

---
+++ infusionsoft-web-tracking-code.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Infusionsoft® Web Tracking Code
+Description: This plugin provides a simple method for Infusionsoft® users to add the Spring Release's new Web Analytics tracking code to their WordPress-powered site.
+Author: Mediajitsu, LLC
+Version: 1.0
+Author URI: http://mediajitsu.com/
+*/
+
+
+
+?>
